8de47b73745905db67ca2c7318a30be0f0551f21
clickclickcode <thanatosanyone@gmail.com>
2026-04-01 17:00:09 +0200

    tilføjer TS type til user.model

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -1,2 +1,9 @@
 // hvordan ser en user ud?
 // dette kan være simpelt i starten
+// dette er kun TS struktur
+
+export interface User {
+    id?: number;
+    username: string;
+    password: string;
+}
